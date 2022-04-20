4bee6eb43b358d67466ff3cbd0933d5229e5ff5b
Leïla MARABESE <lmarabese@scaleway.com>
2022-04-20 10:59:15 +0200

    first commit

---
+++ grpc_test.go
@@ -0,0 +1,27 @@
+package kata_grpc
+
+import (
+	"log"
+	"net"
+	"testing"
+
+	"google.golang.org/grpc"
+)
+
+func initGRPCServer() *grpc.Server {
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+	return grpcServer
+}
+
+func TestGetPizzaFromName(t testing.T) {
+	grpcServer := initGRPCServer()
+
+	response := grpcServer
+}
